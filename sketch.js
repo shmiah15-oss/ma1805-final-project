eafe04a280e758dd36653a53477ed7949fdd2583
Sadiya <shmiah15@gmail.com>
2025-12-01 21:01:53 +0000

    updating

--- sketch.js
+++ sketch.js
@@ -3,6 +3,7 @@ let clouds;
 let button;
 let profile;
 let edit;
+let warning;
 
 let myStatus = 0;
 
@@ -10,6 +11,10 @@ let theText = ["how to write an essay", "how to cite", "synonyms for illustrate"
 let word = theText[0];
 let i = 0;
 
+const NotSecureText ="Not Secure Error Not Secure Error Not Secure Error Not Secure Error Not Secure Error Not Secure Error Not Secure Error Not Secure Error Not Secure Error Not Secure Error Not Secure Error Not Secure Error Not Secure Error Not Secure Errorv Not Secure Error Not Secure Error";
+const NotSecureText2 ="Not Secure Error Not Secure Error Not Secure Error Not Secure Error Not Secure Error Not Secure Error Not Secure Error Not Secure Error Not Secure Error Not Secure Error Not Secure Error Not Secure Error Not Secure Error Not Secure Errorv Not Secure Error Not Secure Error";
+const textArray = NotSecureText.split(" ");
+const textArray2 = NotSecureText2.split(" ");
 
 
 
@@ -18,25 +23,23 @@ function preload() {
   clouds = loadImage('moons-clouds.png');
   profile = loadImage('profile.jpg');
   edit = loadImage('edit.jpg');
+  warning = loadImage('warning.png');
+
 }
 
 function setup() {
   createCanvas(500, 500);
   background(180, 10, 200);
 
-  let button = createButton("search");
-  button.position(370, 170);
+  button = createButton("search");
+  button.position(250, 250);
   button.style('font-family', 'Courier New');
   button.style('background-color', 'black');
   button.style('color', 'white');
   button.mouseClicked(Searching);
 
-
-
   //for the searches
-  frameRate(1);
-
- 
+  frameRate(2);
   
 }
 
@@ -49,10 +52,21 @@ function draw() {
     //startScreen();
   }else if(myStatus==1){
     pageOne();
+    button.position(370, 170);
   }else if (myStatus==2){
     pageTwo();
+    button.position(300, 95);
+    button.style('font-family', 'Courier New')
+    button.style('background-color', 'white');
+    button.style('border', '0px');
+    button.style('color', 'red');
+
   }else if(myStatus==3){
     end();
+    button.position(430, 450);
+    button.style('font-family', 'Courier New');
+    button.style('background-color', 'black');
+    button.style('color', 'white');
   }
 
 
@@ -78,7 +92,7 @@ function Searching() {
 
 
 function startsScreen(){
-  
+//making it look like a browser
 //heading
  textFont('Courier New');
  textSize(50);
@@ -153,47 +167,108 @@ fill(0);
 
 
 
-//
+
 function pageTwo(){
 
-  fill(255);
-textSize(100);
-
-
-//heading
-textFont('Courier New');
-textSize(50);
-fill(255);
-text('ExPLORE', 220, 140);
-
-  image(clouds, 40, 60, 200, 140);
-  image(profile, 420, 5, 100, 60);
-  textSize(15);
-  text('email', 400, 40);
-  fill(255);
-  rect(80, 171, 300, 20);
-  image(edit, 420, 430, 100, 60);
-
-
 
   fill(255);
-  rect(80, 300, 330, 300);
+  rect(80, 50, 330, 300);
 
 
   fill(0);
+  textSize(15);
+  textStyle(BOLD);
+  text('How to cite', 100, 70);
+  textStyle(NORMAL);
   textSize(10);
-  text('How to cite', 120, 60);
+  text('There are many ways to cite, there is...', 105, 80);
 
-  text('How to write an essay', 120, 80);
+  textSize(15);
+  textStyle(BOLD);
+  text('How to write an essay', 100, 110);
+  textStyle(NORMAL);
+  textSize(10);
+  text('An essay is a piece of non-fiction writing...', 105, 120);
+
+  textSize(15);
+  textStyle(BOLD);
+  text('Best ways to study', 100, 150);
+  textStyle(NORMAL);
+  textSize(10);
+  text('10 effective study methods that work are...', 105, 160);
+
+  textSize(15);
+  textStyle(BOLD);
+  text('10 ways to improve writing', 100, 190);
+  textStyle(NORMAL);
+  textSize(10);
+  text('One way to improve your writing is to..', 105, 200);
+
+  textSize(15);
+  textStyle(BOLD);
+  text('Github support', 100, 230);
+  textStyle(NORMAL);
+  textSize(10);
+  text('If there is any issues with your...', 105, 240);
+
+  textSize(15);
+  textStyle(BOLD);
+  text('Making a new page on html', 100, 270);
+  textStyle(NORMAL);
+  textSize(10);
+  text('To make a new page on html, on your vs code..', 105, 280);
+
+  textSize(15);
+  textStyle(BOLD);
+  text('How to create a gif ', 100, 310);
+  textStyle(NORMAL);
+  textSize(10);
+  text('Here are online gif makers that are easy to use...', 105, 320);
+
+
 
 
 
 }
 
 function end(){
-  fill(255);
-  textSize(100);
-  text('oh no! this page is not secure', 200, 100);
+
+  let x = random(width);
+  let y = random(height);
+
+  warning.resize(100, 100); 
+  image(warning, 10, 10, 500, 500);
+  image(warning, x, y,)
+  fill(255, 0, 0);
+  text('NOT SECURE', 250, 100);
+  
+
+
+
+
+  if (i < textArray.length) {
+    fill(255, 0, 0);
+    stroke(100);
+    console.log(i);
+    textSize(50);
+    textAlign(CENTER, CENTER);
+    text(textArray[i], x, y);
+    i++;
+  
+  }
+
+  if (i < textArray2.length) {
+    fill(255, 0, 0);
+    stroke(100);
+    console.log(i);
+    textSize(20);
+    textAlign(CENTER, CENTER);
+    text(textArray2[i], x+100, y+150);
+    i++;
+  
+  }
+
+
 
   
 }
